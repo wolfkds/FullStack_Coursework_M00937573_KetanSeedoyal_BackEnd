563bff5a12341c579b98f3a856111b3d39b5f4ec
Ketan <ketanseedoyalseedoyal@gmail.com>
2024-11-18 21:24:42 +0400

    updated put request

--- index.js
+++ index.js
@@ -153,41 +153,6 @@ app.put("/lessons/:id", async (req, res) => {
   }
 });
 
-
-
-// PUT route to update lesson details
-// app.put("/lessons/:lessonId", async (req, res) => {
-//   try {
-//     const lessonId = req.params.lessonId;
-//     const { space } = req.body; // Only expecting the space field
-
-//     if (space < 0) {
-//       return res.status(400).json({ message: "Space cannot be negative" });
-//     }
-
-//     // Update the space field of the specified lesson
-//     const updatedLesson = await Lesson.findByIdAndUpdate(
-//       lessonId,
-//       { space },
-//       { new: true, runValidators: true } // Return the updated document and enforce validation
-//     );
-
-//     if (!updatedLesson) {
-//       return res.status(404).json({ message: "Lesson not found" });
-//     }
-
-//     res.status(200).json({
-//       message: "Lesson updated successfully",
-//       lesson: updatedLesson,
-//     });
-//   } catch (error) {
-//     console.error("Error updating lesson:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
-
-
-
 // Search functionality for lessons (GET /search)
 app.get("/search", async (req, res) => {
   const query = req.query.q;
